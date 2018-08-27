aa7239d422e1e14cf50ca5db2628eb43aebd3e2a
Jin Kim <jkim1665@gmail.com>
2018-08-27 15:55:14 -0400

    Add files via upload
    made a lot of new changes, primarily fixing the styles

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 var random_result;
 var loss = 0;
 var win = 0;
-
 var previous = 0; // need to make it a number therefore set it equal to 0
 
 var startGame = function () {
 
     $(".crystals").empty();   //reset
 
+    var images =[
+                'assets/images/red.jpg',
+                'assets/images/blue.jpg',
+                'assets/images/green.jpg',
+                'assets/images/yellow.jpg']; 
+
     random_result = Math.floor(Math.random() * 59) + 30;
     //console.log (random_result);
 
-
-
-    $("#result").html('Random Result: ' + random_result);
+    $("#result").html('Random Result: ' + random_result); //adding to the DOM
 
     for (var i = 0; i < 4; i++) {
 
@@ -25,16 +28,30 @@ var startGame = function () {
                 "class": 'crystal',
                 "data-random": randomNum  //how to set your own attribute
             });
+            crystal.css({
+                "background-image":"url('" + images[i] + "')",
+                "background-size":"cover"
+            });
 
-        crystal.html(randomNum); 
-        // cheating to see the numbers
+
+        // crystal.html(randomNum); will display the numbers
 
             $(".crystals").append(crystal);
     }
+
+    $("#previous").html(previous);
+
 }
 
+    
 startGame ();
-//event delegation
+
+
+//event delegation - in jquery not javascript.
+// - when we click the button, not when the page is loaded, this will delete the old element
+// - then the new element will be brought to the DOM
+// - whenever your working with jQuery, the new element the button will no longer be able to listen to any new element that just came into the DOM
+// - the new element will not work, so if u replace it with document and include crystal into the argument. (line 42) 
 $(document).on('click', ".crystal", function () {
 
 
@@ -42,27 +59,26 @@ $(document).on('click', ".crystal", function () {
 
     previous += num;
 
-    console.log(previous);
+    $("#previous").html(previous);
+
+    // console.log(previous);
 
     if (previous > random_result) {
-        loss--;
+        loss++;
         $("#loss").html(loss);
 
         previous = 0;
 
         startGame();
-
     }
     else if (previous === random_result) {
         win++;
-
         $("#win").html(win);
-        
+
         previous = 0;
 
         startGame();
     }
-
 });
 
 
